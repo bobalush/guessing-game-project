1694fa0010e6742c234d2e2041c20141c3741ceb
Liam Dinh <loiphatdinh@gmail.com>
2024-02-14 20:07:18 -0600

    finished the second phase by implementing the askGuess function

--- guessing-game.js
+++ guessing-game.js
@@ -1,18 +1,42 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+});
+
+function askGuess() {
+    rl.question('Enter a guess: ', (num) =>{
+        let guess = Number(num);
+        if (checkGuess(guess)) {
+            console.log('You win!');
+            rl.close();
+        } else {
+            askGuess();
+        }
+    })
+};
+
+
+
 let secretNumber = 5;
 
 function checkGuess(num) {
     if (num > secretNumber ) {
         console.log('Too high.');
         return false;
-    } else if (num < secretNumber) {
+    } 
+    if (num < secretNumber) {
         console.log('Too low.');
         return false;
-    } else {
+    }
+    if (num === secretNumber) {
         console.log('Correct');
         return true;
     }
 }
 
-checkGuess(10);
+/* checkGuess(10);
 checkGuess(3);
-checkGuess(5);
+checkGuess(5); */
+askGuess();
