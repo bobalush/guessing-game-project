8b4a3b3fcf096774117a7f3a0d836d6da978baa4
Liam Dinh <loiphatdinh@gmail.com>
2024-02-17 20:27:30 -0600

    implementing askRange function, along with the limiting function and turned the limiting function dynamically asking user to input the limit themself to make the game more interesting.

--- guessing-game.js
+++ guessing-game.js
@@ -5,23 +5,48 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function askGuess() {
+//let attemptsLeft = 5;
+function askLimit() {
+    rl.question('Enter the nubmer of attempts for the game: ', (limitInput) => {
+        const attemptsLeft = Number(limitInput);
+        askRange(attemptsLeft);
+    })
+}
+function askRange(attemptsLeft) {
+    rl.question('Enter a min number: ', (min) => {
+        rl.question('Enter a mx number: ', (max) => {
+            min = Number(min);
+            max = Number(max); 
+            console.log(`I'm thinking of a number between ${min} and ${max}...`);
+
+            const secretNumber = randomInRange(min, max);
+
+            askGuess(secretNumber, attemptsLeft);
+        })
+    })
+}
+
+function askGuess(secretNumber, attemptsLeft) {
+    if (attemptsLeft <=0) {
+        console.log('Sorry, you ran out of attempts. Game over!');
+        rl.close();
+        return;
+    }
+
     rl.question('Enter a guess: ', (num) =>{
         let guess = Number(num);
-        if (checkGuess(guess)) {
+        if (checkGuess(guess, secretNumber)) {
             console.log('You win!');
             rl.close();
         } else {
-            askGuess();
+            askGuess(secretNumber, attemptsLeft - 1);
+            //console.log(attemptsLeft);
         }
-    })
+    });
 };
 
 
-
-let secretNumber = randomInRange(0, 100);
-
-function checkGuess(num) {
+function checkGuess(num, secretNumber) {
     if (num > secretNumber ) {
         console.log('Too high.');
         return false;
@@ -39,11 +64,8 @@ function checkGuess(num) {
 function randomInRange(num1, num2) {
     return Math.floor(Math.random()*(num2 - num1 + 1) + num1);
 }
-/* checkGuess(10);
-checkGuess(3);
-checkGuess(5); */
-askGuess();
 
-/* console.log(randomInRange(15, 20)); // 16
-console.log(randomInRange(15, 20)); // 17
-console.log(randomInRange(15, 20)); // 20 */
+
+
+//askRange();
+askLimit();
